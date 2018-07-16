3172f4b80dce273340e9dab333a2dc4c1a6fe3ba
CN <857353825@qq.com>
2018-07-16 23:13:38 +0800

    部分并发测试

--- self/D0716.java
+++ self/D0716.java
@@ -4,15 +4,50 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Method;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class D0716 {
     
-    public static void main(String[] args) {
-        TestProcessor.process(NeedTest.class);
+    public static void main(String[] args) throws InterruptedException {
+        // TestProcessor.process(NeedTest.class);
+
+        // MyThread thread = new MyThread();
+        // thread.start();
+
+        // LiftOff off = new LiftOff();
+        // off.run();
+        // System.out.println("AAA");
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+        // ExecutorService exec = Executors.newFixedThreadPool(3);
+        for(int i=0; i<5; i++)
+            exec.execute(new LiftOff());
+        // Thread.sleep(500);
+        System.out.println("Hello ");
+        exec.shutdown();
     }
     
 }
 
+class LiftOff implements Runnable {
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++)
+            System.out.println("["+id+"]("+Thread.currentThread().getId()+") " + i);
+        System.out.println("["+id+"] QAQ");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for(int i=10; i<20; i++) 
+            System.out.println(i);
+    }
+}
 
 class NeedTest {
     @Test
@@ -22,14 +57,14 @@ class NeedTest {
 }
 
 /**
- * Ä£ÄâJUnitÊµÏÖµ¥Ôª²âÊÔ
+ * æ¨¡æ‹ŸJUnitå®žçŽ°å•å…ƒæµ‹è¯•
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @interface Test {}
 
 /**
- * @Test×¢½â´¦ÀíÆ÷
+ * @Testæ³¨è§£å¤„ç†å™¨
  */
 class TestProcessor {
     public static void process(Class cl) {
